49a7ab114004f1284fcde7593e68ca660de90ba9
Weidsom Nascimento <source@snakesecurity.org>
2025-04-02 10:06:00 -0700

    Add commands to remove old sqlcipher schema

--- AFOSBUILD.sh
+++ AFOSBUILD.sh
@@ -1,3 +1,6 @@
+rm -rf /usr/local/lib/libsqlcipher.*
+rm -rf /usr/local/bin/sqlcipher
+
 ./configure --with-tempstore=yes CFLAGS="-DSQLITE_HAS_CODEC -DSQLITE_EXTRA_INIT=sqlcipher_extra_init -DSQLITE_EXTRA_SHUTDOWN=sqlcipher_extra_shutdown" --disable-tcl LDFLAGS="-lcrypto -lssl"
 
 if [ $? -eq 0 ]
