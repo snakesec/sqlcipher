2089e5c7b6077dd0c3c4185831d046d6049b4e1e
Weidsom Nascimento <source@snakesecurity.org>
2025-04-02 09:59:33 -0700

    Add extra LD flags

--- AFOSBUILD.sh
+++ AFOSBUILD.sh
@@ -1,4 +1,4 @@
-./configure --with-tempstore=yes CFLAGS="-DSQLITE_HAS_CODEC -DSQLITE_EXTRA_INIT=sqlcipher_extra_init -DSQLITE_EXTRA_SHUTDOWN=sqlcipher_extra_shutdown" --disable-tcl
+./configure --with-tempstore=yes CFLAGS="-DSQLITE_HAS_CODEC -DSQLITE_EXTRA_INIT=sqlcipher_extra_init -DSQLITE_EXTRA_SHUTDOWN=sqlcipher_extra_shutdown" --disable-tcl LDFLAGS="-lcrypto -lssl"
 
 if [ $? -eq 0 ]
 then
